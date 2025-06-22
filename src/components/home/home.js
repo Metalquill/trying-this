ddbb804b6938cbc2b5e3dd4c3ddf1d7ace7f4815
Metalquill <65878989+Metalquill@users.noreply.github.com>
2025-06-23 10:27:50 +1200

    home component update

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,16 +4,28 @@ import { Link } from 'react-router-dom'
 import DayNight from '../dayNight/dayNight'
 
 export class home extends Component {
+
+    
   render() {
     return (
-      <div>
+      <div className='home-container'>
+         <div className='day-n-night'>
+          <DayNight />
+        </div>
         <div className="card">
             <h3>Home</h3>
             <Link to='/cat'><h5>Are you a Cat?</h5></Link>
             <Link to='/weather'><h5 className='weather-link'>Weather</h5></Link>
         </div>
-        <div className='day-n-night'>
-          <DayNight />
+        <div className='mynav-container'>
+          <div className='card'>
+            <h5>Nav bar</h5>
+            <ul>
+              <li> <Link to='/cat'>Cat</Link></li>
+              <li><Link to='/weather'>Weather</Link></li>
+              <li><Link to='/timesheet'>Timesheet</Link></li>
+            </ul>
+          </div>
         </div>
       </div>
     )
