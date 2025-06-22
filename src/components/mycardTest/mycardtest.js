2a05f5802c419e4a4850bb193d1ad069e5cd4daf
Metalquill <65878989+Metalquill@users.noreply.github.com>
2025-06-23 10:22:19 +1200

    new card test component

---
+++ src/components/mycardTest/mycardtest.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import chch from '../images/chch.jpg'
+import './mycardTest.css'
+
+export default function MyCardTest() {
+    return (
+             <div className='new-card-container'>
+          <div className='card'>
+            <img className='image' src={chch} />
+            <div class="content">
+              <h5 className='myh5'>Christchurch</h5>
+              <p>Lorem ipsum</p>
+              <a className='read-more'> Read more </a>
+              <div className='details'>
+                <span className='item'>
+                  <span className='material...'>sell</span>
+                <em>from $120</em>
+                </span>
+                <span className='item'>
+                  <span className='material...'>travel</span>
+                  <em>Chch</em>
+                </span>
+              </div>
+              <div className='my-buttons'>
+                <button className='primary-btn'>Search</button>
+                <button className='icon-btn'>
+                  <span className='material...'>favourite</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+    )
+}
