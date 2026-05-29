07a534034ba90e8cae39ddfdc5eaf5c0c694d122
Metalquill <65878989+Metalquill@users.noreply.github.com>
2026-05-29 14:51:44 +1200

    Updated myCardTest

--- src/components/mycardTest/mycardtest.js
+++ src/components/mycardTest/mycardtest.js
@@ -1,8 +1,53 @@
 import React from 'react'
 import chch from '../images/chch.jpg'
 import './mycardTest.css'
+// import {
+//   Chart as ChartJS,
+//   ArcElement,
+//   Tooltip,
+//   Legend,
+// } from 'chart.js';
+// import { Doughnut } from 'react-chartjs-2';
+
+
 
 export default function MyCardTest() {
+
+  // ChartJS.register(ArcElement, Tooltip, Legend);
+
+  // const doughnutChart = () => {
+    
+  // }
+
+  // const data = {
+  //   labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  //   datasets: [
+  //     {
+  //       label: '# of Votes',
+  //       data: [12, 19, 3, 5, 2, 3],
+  //       backgroundColor: [
+  //         'rgba(255, 99, 132, 0.2)',
+  //         'rgba(54, 162, 235, 0.2)',
+  //         'rgba(255, 206, 86, 0.2)',
+  //         'rgba(75, 192, 192, 0.2)',
+  //         'rgba(153, 102, 255, 0.2)',
+  //         'rgba(255, 159, 64, 0.2)',
+  //       ],
+  //       borderColor: [
+  //         'rgba(255, 99, 132, 1)',
+  //         'rgba(54, 162, 235, 1)',
+  //         'rgba(255, 206, 86, 1)',
+  //         'rgba(75, 192, 192, 1)',
+  //         'rgba(153, 102, 255, 1)',
+  //         'rgba(255, 159, 64, 1)',
+  //       ],
+  //       circumference: 180,
+  //       rotation: 270,
+  //       borderWidth: 1,
+  //     },
+  //   ],
+  // };
+
     return (
              <div className='new-card-container'>
           <div className='card'>
@@ -30,6 +75,12 @@ export default function MyCardTest() {
             </div> */}
             <div className="content">
               <ul>
+                <li><a href='https://ucdigitalsms.atlassian.net/wiki/spaces/SFP/pages/3265560588/UC+Git+Tooling' target="_blank" rel="noreferrer">Git Tooling</a></li>
+                <li><a href="https://ucdigitalsms.atlassian.net/wiki/spaces/SFP/pages/2279702618/Git+Gud+-+A+little+help+on+using+Git+in+Student+First" target="_blank" rel="noreferrer">Git Gud</a></li>
+                <li><a href='https://ucdigitalsms.atlassian.net/wiki/spaces/SFP/pages/2045214731/Developer+Portal' target="_blank" rel="noreferrer">Developer portal</a></li>
+                <li><a href='https://ucdigitalsms.atlassian.net/wiki/spaces/SFP/pages/2388492302/System+outage+and+banners' target="_blank" rel="noreferrer">Banners</a></li>
+                <li><a href='https://ucdigitalsms.atlassian.net/wiki/spaces/SFP/pages/2422276097/The+ultimate+guide+and+glossary+of+acronyms' target="_blank" rel="noreferrer">Glossary</a></li>
+                <li><a href='https://ucdigitalsms.atlassian.net/wiki/spaces/SFP/pages/2618229350/The+Course+Data+Loader' target='_blank' rel='noreferrer'>Course loader</a></li>
               </ul>
             </div>
           </div>
@@ -37,6 +88,6 @@ export default function MyCardTest() {
     )
 }
 
-
-
-
+// sudo password: cat
+// Sunflower1977!!
+// ticket CookiesAndCream42
